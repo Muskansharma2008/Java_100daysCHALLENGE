523af208366f3c32d729c6d6c9fa24c2ea2e9d85
Muskan Sharma <muskansharmanis@gmail.com>
2026-08-12 23:55:18 +0530

    Day 3: Operators

--- Day3_operators.java
+++ Day3_operators.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
+
 public class Day3_operators {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a: ");
-        int a= sc.nextInt();
+        int a = sc.nextInt();
         System.out.println("Enter b: ");
-        int b= sc.nextInt();
-        System.out.println("Enter any value6 between 1 and 4: ");
-        int ch= sc.nextInt();
-        if((ch>=1 || ch<=4) && ch>0) {
-            switch(ch) {
+        int b = sc.nextInt();
+        System.out.println("Enter any value between 1 and 4: ");
+        int ch = sc.nextInt();
+        if ((ch >= 1 || ch <= 4) && ch > 0) {
+            switch (ch) {
                 case 1:
-                System.out.println("Addition: " + (a+b));
+                    System.out.println("Addition: " + (a + b));
                 case 2:
-                System.out.println("Subtraction: " + (a-b));
+                    System.out.println("Subtraction: " + (a - b));
                 case 3:
-                System.out.println("Multiplication: " + (a*b));
+                    System.out.println("Multiplication: " + (a * b));
                 case 4:
-                System.out.println("Division: " + (a/b));
+                    System.out.println("Division: " + (a / b));
             }
-        }
-        else {
+        } else {
             System.out.println("Invalid Input");
         }
     }
