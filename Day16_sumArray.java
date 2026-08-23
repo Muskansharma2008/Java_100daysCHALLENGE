d284c415e693ecb42265d8b6b11de141c68f6e73
Muskan Sharma <muskansharmanis@gmail.com>
2026-08-23 21:50:33 +0530

    Day 16: Sum of array

---
+++ Day16_sumArray.java
@@ -0,0 +1,10 @@
+public class Day16_sumArray {
+    public static void main(String[] args) {
+        int sum = 0;
+        int arr[] = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Sum of array: " + sum);
+    }
+}
