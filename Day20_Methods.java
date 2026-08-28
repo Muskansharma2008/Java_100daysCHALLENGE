c520ed3d68f7664ea45273b5375954e7046054b8
Muskan Sharma <muskansharmanis@gmail.com>
2026-08-28 23:33:37 +0530

    Day 20: Methods

---
+++ Day20_Methods.java
@@ -0,0 +1,11 @@
+public class Day20_Methods {
+
+    public void Sum(int a, int b) {
+        System.out.println("Sum : " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        Day20_Methods m = new Day20_Methods();
+        m.Sum(7, 2);
+    }
+}
