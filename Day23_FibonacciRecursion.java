31b25c4f12f44369c857e6366a3ae6d7a3726e49
Muskan Sharma <muskansharmanis@gmail.com>
2026-08-31 20:59:48 +0530

    Day 23: Fibonacci using Recursion

---
+++ Day23_FibonacciRecursion.java
@@ -0,0 +1,18 @@
+public class Day23_FibonacciRecursion {
+
+    static int fib(int n) {
+        if (n <= 1)
+            return n;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 7;
+        System.out.println("Fibonacci series :");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+    }
+}
