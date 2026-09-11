44f474b767a682784dc0db19743c57ce6e029975
Muskan Sharma <muskansharmanis@gmail.com>
2026-09-11 23:10:40 +0530

    Day 31: Binary search

--- Day31_BinarySearch.java
+++ Day31_BinarySearch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Day30_BinarySearch {
+public class Day31_BinarySearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
