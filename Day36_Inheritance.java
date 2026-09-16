735b2d14eb16f3db761bce7ee95060c7df3fce5b
Muskan Sharma <muskansharmanis@gmail.com>
2026-09-16 22:33:24 +0530

    Day 36: Inheritance

--- Day36_Inheritance.java
+++ Day36_Inheritance.java
@@ -4,16 +4,16 @@ class Animal {
     }
 }
 
-class Dog extends Animal {
-    void bark() {
-        System.out.println("Barks");
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Meow");
     }
 }
 
 public class Day36_Inheritance {
     public static void main(String[] args) {
-        Dog a = new Dog();
+        Cat a = new Cat();
         a.sound();
-        a.bark();
+        a.meow();
     }
 }
