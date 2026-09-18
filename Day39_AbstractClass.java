1b358b77973becc4843d8bf959ca25af570770b6
Muskan Sharma <muskansharmanis@gmail.com>
2026-09-18 23:37:45 +0530

    Day 39: Abstract class

---
+++ Day39_AbstractClass.java
@@ -0,0 +1,26 @@
+abstract class Animal {
+
+    abstract void sound();
+
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Day39_AbstractClass {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.sound();
+        d.eat();
+    }
+}
