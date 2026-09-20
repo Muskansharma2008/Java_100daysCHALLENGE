5f6f7edf79b5bbee7f546deec01ae18c31e657a1
Muskan Sharma <muskansharmanis@gmail.com>
2026-09-20 22:10:55 +0530

    Day 41: Package

---
+++ Day41_CustomPackage.java
@@ -0,0 +1,10 @@
+import mypackage.Calculator;
+
+public class Day41_CustomPackage {
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        c.add(10, 5);
+    }
+}
